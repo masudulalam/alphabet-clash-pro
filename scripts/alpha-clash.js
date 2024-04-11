aa9e59652dcd35665ea17aec3d78ed6f7c49d9fa
masudulalam <masuudulalam@gmail.com>
2024-04-11 23:52:09 +0600

    added random alphabet displaying

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -15,6 +15,10 @@ function continueGame () {
     // step - 1 : Generate a random alphabet.
     const alphabet = getArandomAlphabet();
     console.log('Your random alphabet',alphabet);
+
+    // set randomly generated alphabet to the screen for display.
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
 }
 
 function play () {
