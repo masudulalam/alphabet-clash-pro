f35cbe5e612f2b11642507de0bc1ee804a541ac4
masudulalam <masuudulalam@gmail.com>
2024-04-12 00:12:01 +0600

    add random alphabet keyboard highlight

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -19,6 +19,9 @@ function continueGame () {
     // set randomly generated alphabet to the screen for display.
     const currentAlphabetElement = document.getElementById('current-alphabet');
     currentAlphabetElement.innerText = alphabet;
+
+    // set background color
+    setBackgroundColorById(alphabet);
 }
 
 function play () {
